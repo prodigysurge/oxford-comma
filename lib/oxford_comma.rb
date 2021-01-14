47ac0151c67e6206de00eaae551fe77985e28e2c
prodigysurge <boatinglife6@yahoo.com>
2021-01-13 20:57:23 -0500

    commit tree 80cba6460ed737901c5a96d354677748c279f831

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+# resource stackoverflow.com
+
 def oxford_comma(array)
+    case array.size
+    when 0
+        "" # does nothing
+    when 1
+        array.first # only returns the first of the array
+    when 2
+        array.join(" and ") # joins the 2 elements with "and"
+    else
+        [array[0..-2].join(", "),array.last].join(", and ") # seperates all elements with a comma, in addition to this adds an "and" before the -1 index
+    end
 
 end
